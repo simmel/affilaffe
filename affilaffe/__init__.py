6100a6821d3da04bbdd92e90040cdce5efefc7f2
Simon Lundström <simlu+github@su.se>
2019-09-18 13:32:39 +0200

    Add ldap3

--- affilaffe/__init__.py
+++ affilaffe/__init__.py
@@ -2,6 +2,7 @@
 # pylint: disable=missing-docstring,import-error,invalid-name
 
 import ldap
+import ldap3
 import ldap.sasl
 
 
@@ -19,9 +20,27 @@ def python_ldap():
     print(result)
 
 
+def ldaptre():
+    print("ldap3")
+    server = ldap3.Server('ldaps://ldap.su.se', get_info=ldap3.ALL)
+    conn = ldap3.Connection(
+        server,
+        auto_bind=True,
+        authentication=ldap3.SASL,
+        sasl_mechanism=ldap3.GSSAPI,
+    )
+    print("whoami: {}".format(conn.extend.standard.who_am_i()))
+    conn.search(search_base='',
+                search_scope=ldap3.SUBTREE,
+                search_filter='(uid=simlu)',
+                attributes=['eduPersonAffiliation'])
+    print(conn.entries)
+
+
 def main():
     python_ldap()
     print("")
+    ldaptre()
 
 
 if __name__ == "__main__":
