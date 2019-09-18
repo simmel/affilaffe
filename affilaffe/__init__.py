c035a9b0d2cbe6acd34fb6fef2d67fb81927209e
Simon Lundström <simlu+github@su.se>
2019-09-18 12:59:51 +0200

    Add python-ldap implementation

--- affilaffe/__init__.py
+++ affilaffe/__init__.py
@@ -1,8 +1,27 @@
-__version__ = '0.1.0'
+#!/usr/bin/env python3
+# pylint: disable=missing-docstring,import-error,invalid-name
+
+import ldap
+import ldap.sasl
+
+
+def python_ldap():
+    print("python-ldap")
+    l = ldap.initialize("ldaps://ldap.su.se")  # noqa: E741
+
+    l.sasl_gssapi_bind_s()
+    print("whoami: {}".format(l.whoami_s()))
+
+    result = l.search_s(base="",
+                        scope=ldap.SCOPE_SUBTREE,
+                        filterstr="(uid=simlu)",
+                        attrlist=["eduPersonAffiliation"])
+    print(result)
 
 
 def main():
-    print("afffeeee")
+    python_ldap()
+    print("")
 
 
 if __name__ == "__main__":
